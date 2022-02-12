b398f4756d813887699e03bfd581c5af6ab44c3e
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-02-12 15:40:54 +0900

    fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,37 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+  const cache = {}
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args]
+    }
 
-module.exports = fib;
+    const result = fn.apply(this, args)
+    cache[args] = result
+    return result
+  }
+}
+
+function slowFib(n) {
+  // 再帰的な処理
+  if (n < 2) {
+    return n
+  }
+  return fib(n - 1) + fib(n - 2)
+}
+
+const fib = memoize(slowFib)
+
+// const result = [0, 1]
+
+// for (let i = 2; i <= n; i++) {
+//   // 配列数を取得しておく。
+//   const a = result[i - 1]
+//   const b = result[i - 2]
+//   result.push(a + b)
+// }
+// return result[n]
+// return result[results.length - 1]
+module.exports = fib
